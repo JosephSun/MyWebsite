587c812d5ab989434adf43d1203fe2227a85fa0b
BKhurshid <josephsun4@gmail.com>
2016-04-02 16:38:52 -0700

    reloved extensions of jsx

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,13 @@ module.exports = {
     publicPath: '/'
   },
 
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
   module: {
     loaders: [
-      { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
     ]
   }
 }
