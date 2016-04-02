2ebcf6b08000d2df2b469783e56e15ff229df7c5
BKhurshid <josephsun4@gmail.com>
2016-04-02 15:40:39 -0700

    React-router tutorial help

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,14 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + '/app/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
-
 module.exports = {
-  entry: [
-    './app/index.jsx'
-  ],
-  resolve: {
-    extentions: ['', '.js', '.jsx']
+  entry: './index.js',
+
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/'
   },
+
   module: {
     loaders: [
-      {test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader"}
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
     ]
-  },
-  output: {
-    filename: "index_bundle.js",
-    path: __dirname + '/dist'
   }
-};
+}
