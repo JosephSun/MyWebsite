4142e98e209a4e38d91516033d57c460b8fd5728
BKhurshid <josephsun4@gmail.com>
2016-04-02 18:58:59 -0700

    added classnames to li instead of navlink

--- components/App.jsx
+++ components/App.jsx
@@ -7,10 +7,10 @@ export default React.createClass({
       <div>
         <h1>React Router Tutorial</h1>
         <ul role="nav" className="nav-bar">
-          <li><NavLink to="/" className="nav-constant" onlyActiveOnIndex>Home</NavLink></li>
-          <li><NavLink to="/about" className="nav-constant">About</NavLink></li>
-          <li><NavLink to="/repos" className="nav-constant">Repos</NavLink></li>
-          <li><NavLink to="/blogs" className="nav-constant">Blogs</NavLink></li>
+          <li className="nav-constant"><NavLink to="/"  onlyActiveOnIndex>Home</NavLink></li>
+          <li className="nav-constant"><NavLink to="/about" >About</NavLink></li>
+          <li className="nav-constant"><NavLink to="/repos" >Repos</NavLink></li>
+          <li className="nav-constant"><NavLink to="/blogs" >Blogs</NavLink></li>
         </ul>
         {this.props.children}
       </div>
