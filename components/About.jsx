38a3dcffbafa22058ac3f51d6a7b4f2ed9174f24
BKhurshid <josephsun4@gmail.com>
2016-04-03 16:38:47 -0700

    moved text to p tag

--- components/About.jsx
+++ components/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default React.createClass({
   render() {
-    return <div className="content">About</div>
+    return (
+      <div className="content">
+
+        <p className="about-text">About</p>
+
+
+      </div>
+    )
   }
 })
