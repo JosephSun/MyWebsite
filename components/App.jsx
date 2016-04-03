df2bceff388b71f8741b9cd9510c26e6aa4e0bc9
BKhurshid <josephsun4@gmail.com>
2016-04-02 19:53:33 -0700

    added classnames

--- components/App.jsx
+++ components/App.jsx
@@ -5,12 +5,11 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <h1>React Router Tutorial</h1>
         <ul role="nav" className="nav-bar">
-          <li className="nav-constant"><NavLink to="/"  onlyActiveOnIndex>Home</NavLink></li>
-          <li className="nav-constant"><NavLink to="/about" >About</NavLink></li>
-          <li className="nav-constant"><NavLink to="/repos" >Repos</NavLink></li>
-          <li className="nav-constant"><NavLink to="/blogs" >Blogs</NavLink></li>
+          <li className="nav-constant"><NavLink to="/" className="nav-text"  onlyActiveOnIndex>Home</NavLink></li>
+          <li className="nav-constant"><NavLink to="/about" className="nav-text" >About</NavLink></li>
+          <li className="nav-constant"><NavLink to="/repos" className="nav-text" >Repos</NavLink></li>
+          <li className="nav-constant"><NavLink to="/blogs" className="nav-text" >Blogs</NavLink></li>
         </ul>
         {this.props.children}
       </div>
