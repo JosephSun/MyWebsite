13d7eead7b7012329f0371b070c39b0d5235b169
BKhurshid <josephsun4@gmail.com>
2016-04-03 14:31:14 -0700

    avoiding the nav-bar

--- components/Repos.jsx
+++ components/Repos.jsx
@@ -5,7 +5,7 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <h2>Repos</h2>
+        <h2 className="avoiding-navbar">Repos</h2>
         <ul className="avoiding-navbar">
           <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
