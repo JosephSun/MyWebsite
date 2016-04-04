04f66072f727ca1887707ef81c615c4da8c2367e
BKhurshid <josephsun4@gmail.com>
2016-04-03 20:16:01 -0700

    changed class name

--- components/NavLink.jsx
+++ components/NavLink.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default React.createClass({
   render() {
-    return <Link {...this.props} className="content" activeClassName="active"/>
+    return <Link {...this.props} activeClassName="active"/>
   }
 })
