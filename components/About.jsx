fb8630f8bece82df9f1f17fd26b5c6d7c737945f
BKhurshid <josephsun4@gmail.com>
2016-04-04 20:31:46 -0700

    modifyinig text

--- components/About.jsx
+++ components/About.jsx
@@ -10,7 +10,7 @@ export default React.createClass({
         <div className="container-about-text">
           <h1>About the guy in the circle</h1>
           <p>
-            His name is Joseph Sun and I am him or he is me. I'm never quite sure how to phrase that, which must mean that neither does he. Huh. I am a full stack software engineer who focuses primarily on React and Node. My previous experience includes building web applications using Express, Node and Angular, among other technologies. You can click the word resume at the top of the website, or if your feeling lazy, you can just click <NavLink to="/repos">here</NavLink>.
+            His name is Joseph Sun and I am him or he is me. I'm never quite sure how to phrase that, which must mean that neither does he. Huh. I am a full stack software engineer who focuses primarily on React and Node. My previous experience includes building web applications using Express, Node and Angular, among other technologies. If you want a more detailed version of my experience, you can click the word resume at the top of the website or, if your feeling lazy, you can just click <NavLink to="/repos">here</NavLink>.
 
           </p>
           <img src="../pics/FacePic.jpg" alt="Profile Picture" className="profile-pic-two" height="42" width="42"/>
@@ -18,10 +18,10 @@ export default React.createClass({
           <h1>About the wierdo in the square</h1>
 
           <p>
-              His passions include building with a purpose and increasing his productivity (like I said, wierdo-I mean, look at how he refers to himself in the third person). He likes to dabble in frontend architecture from time to time but mostly just focuses on buildiing features.
+              His passions include building with a purpose and increasing his productivity (like I said, wierdo-I mean, look at how he refers to himself in third person). He likes to dabble in frontend architecture from time to time but mostly just focuses on buildiing features.
               <br/>
               <br/>
-              This is the part where I would list my good qualities (did the switch between third person to first person confuse you? It sure confused him) but I'm pretty sure you have heard it all before. So, I'll let you fill in the blank. I am _____. That is why you should _____. Hopefully you filled in both blanks with good things.
+              This is the part where I would list my good qualities (did the switch between third person to first person confuse you? It sure confused him) but I'm pretty sure everyone says the same thing (have you ever met someone who has claimed they are not hard working?). So, I'll let you fill in the blank. I am _____. That is why you should _____. Hopefully you filled in both blanks with good things.
             <br/>
             <br/>
             Feel free to contact me and/or check out my work. You can find me on Github, Twitter and <Link to="https://www.linkedin.com/in/josephsun4">LinkedIn</Link>.
