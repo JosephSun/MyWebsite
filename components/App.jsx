300f697b5e9b734cc0f9f4563934bf688cc23853
BKhurshid <josephsun4@gmail.com>
2016-04-04 20:10:16 -0700

    up

--- components/App.jsx
+++ components/App.jsx
@@ -8,7 +8,7 @@ export default React.createClass({
         <ul role="nav" className="nav-bar">
           <li className="nav-constant"><NavLink to="/" className="nav-text"  onlyActiveOnIndex>Home</NavLink></li>
           <li className="nav-constant"><NavLink to="/about" className="nav-text" >About</NavLink></li>
-          <li className="nav-constant"><NavLink to="/repos" className="nav-text" >Repos</NavLink></li>
+          <li className="nav-constant"><NavLink to="/repos" className="nav-text" >Resume</NavLink></li>
           <li className="nav-constant"><NavLink to="/blogs" className="nav-text" >Blogs</NavLink></li>
         </ul>
         {this.props.children}
