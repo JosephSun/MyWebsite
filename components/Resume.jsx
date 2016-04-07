4d42987eb6666b259b5bcc615af8ca27fd4b7e92
BKhurshid <josephsun4@gmail.com>
2016-04-07 15:00:24 -0700

    Added format to resume

--- components/Resume.jsx
+++ components/Resume.jsx
@@ -5,7 +5,27 @@ export default React.createClass({
   render() {
     return (
       <div className="content">
-        Blogs go here!
+        <div>
+          <h4>Technical Skills</h4>
+            <p> Strong: Javascript, Node.js, Express, Angular.js, Socket.io, Git<p>
+            <p> Experienced: React, Webpack, Babel, ES6, Backbone.js, D3, jQuery, Passport.js, Heroku<p>
+        </div>
+        <div>
+          <h4>Projects</h4>
+
+        </div>
+        <div>
+          <h4>Experience</h4>
+
+        </div>
+        <div>
+          <h4>Education</h4>
+
+        </div>
+        <div>
+          <h4>Personal</h4>
+
+        </div>
       </div>
     )
   }
