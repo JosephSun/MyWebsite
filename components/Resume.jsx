df9ad5a6f9889d021f3e0ef11133ea4289e6d944
BKhurshid <josephsun4@gmail.com>
2016-04-07 15:22:21 -0700

    adding further informed

--- components/Resume.jsx
+++ components/Resume.jsx
@@ -47,42 +47,75 @@ export default React.createClass({
             </ul>
 
             <p> <Link to="https://github.com/JosephSun/booted" target="_blank">Booted</Link> | Frontend Engineer | Scrum Master | </p>
-            <p> World-Wide Pair Programming WebApp </p>
 
-            <ul>
+          <p> Ranking system for Software Engineering programs </p>
+
+          <ul>
+
               <li>
-                Transmitted real-time messaging and code collaboration with Websockets
+                Implemented voting system with Angular.js
               </li>
 
               <li>
-                Architected client side and server logic for storing, displaying user profiles and documents
+                Coached team through product development using agile like methodology, such as rapid iteration and sprints
               </li>
 
               <li>
-                Created RESTful API endpoints to handle client HTTP requests using Node.js and Express
+                Spearheaded research into various APIs, informing final product choice.
               </li>
 
               <li>
-                Persisted, queried, updated and deleted information with MongoDB
+                Created a search engine using Custom Google Search API
               </li>
 
               <li>
-                Implemented user authentication with Github OAuth2 and Passport.js
-              </li>
-              <li>
-
-                Developed logic for uploading files using Node.js
+                Managed Git repository and scrum-centered backlog for a four person team using Waffle.io
               </li>
 
+            </ul>
+
+            <p> <Link to="https://github.com/JosephSun/Colorado-Eco-Corporation" target="_blank">Colorado Eco-Corporation</Link> | Frontend Engineer </p>
+
+          <p>  Allows Coloradans to stay informed about their business eco-system </p>
+
+          <ul>
               <li>
-                Modularized codebase, improving team productivity and decreasing version control conflicts
+                Constructed single-page application with Angular.js and rendered information to user through client side routing
+              </li>
+
+              <li>
+                Consolidated Colorado’s corporation charters through RESTfull calls
+              </li>
+
+              <li>
+                Formulated the client side logic which parsed data received from external Government API calls using Angular.js
               </li>
 
             </ul>
         </div>
-        <div>
-          <h4>Experience</h4>
 
+        <div>
+
+          <h4>Experience</h4>
+          
+            <p> <Link to="https://github.com/JosephSun/Colorado-Eco-Corporation" target="_blank">Colorado Eco-Corporation</Link> | Frontend Engineer </p>
+
+            <p>  Allows Coloradans to stay informed about their business eco-system </p>
+
+            <ul>
+                <li>
+                  Constructed single-page application with Angular.js and rendered information to user through client side routing
+                </li>
+
+                <li>
+                  Consolidated Colorado’s corporation charters through RESTfull calls
+                </li>
+
+                <li>
+                  Formulated the client side logic which parsed data received from external Government API calls using Angular.js
+                </li>
+
+              </ul>
         </div>
         <div>
           <h4>Education</h4>
