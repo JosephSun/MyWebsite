b24c9dfad1e79f34627ff5acea0596b69da7dedd
BKhurshid <josephsun4@gmail.com>
2016-04-09 13:08:34 -0700

    concat js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
-const gulp = require('gulp');
+const gulp = require('gulp'),
+concat = require('gulp-concat');
 
-gulp.task('default', () => {
 
-});
+// creating our task named test
+gulp.task('test', () => {
+  console.log("Gulp is running");
+})
+
+gulp.task('concatScripts', () => {
+  gulp.src([
+    'js/jquery.js',
+    'js/main.js'
+  ])
+  .pipe(concat("app.js"))
+  .pipe(gulp.dest('js'))
+})
