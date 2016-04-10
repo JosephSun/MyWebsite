686616a49e1023782b910f9759312fd5f14f0abd
BKhurshid <josephsun4@gmail.com>
2016-04-10 14:11:14 -0700

    delted lines

--- server.js
+++ server.js
@@ -14,14 +14,3 @@ app.get('*',  (req, res) => {
 app.listen(PORT, () => {
   console.log(`Production Express server running at localhost: + ${PORT})`)
 });
-
-
-// ...
-// add path.join here
-app.use(express.static(path.join(__dirname, 'public')))
-
-// ...
-app.get('*', function (req, res) {
-  // and drop 'public' in the middle of here
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
