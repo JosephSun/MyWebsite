0ba8257385daf9e9580d874ee2d770523801a1b5
BKhurshid <josephsun4@gmail.com>
2016-04-10 13:59:16 -0700

    listening to port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const compression = require('compression');
 const express = require('express');
 const path = require('path');
-
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -10,3 +10,7 @@ app.use(express.static(__dirname));
 app.get('*',  (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 })
+
+app.listen(PORT, () => {
+  console.log(`Production Express server running at localhost: + ${PORT})`)
+});
