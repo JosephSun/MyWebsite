9674c0fd76c665017cfb51e88a29064b5c07f1e1
BKhurshid <josephsun4@gmail.com>
2016-04-10 15:38:47 -0700

    don't want to show these yet

--- components/Repos.jsx
+++ components/Repos.jsx
@@ -2,13 +2,14 @@ import NavLink from './NavLink'
 import React from 'react'
 
 export default React.createClass({
+  // <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
+  // <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+
   render() {
     return (
       <div>
         <h2 className="content">Repos</h2>
         <ul className="content">
-          <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
-          <li><NavLink to="/repos/facebook/react">React</NavLink></li>
         </ul>
         {this.props.children}
       </div>
