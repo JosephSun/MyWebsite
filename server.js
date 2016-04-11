305e950665a34c32c257659279dc81d6f2e5983a
BKhurshid <josephsun4@gmail.com>
2016-04-10 19:41:44 -0700

    changing file path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*',  (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.listen(PORT, () => {
