3e3b1091a1992abe0cff8a774cfd7de2c1db745a
BKhurshid <josephsun4@gmail.com>
2016-04-10 19:34:33 -0700

    moved props.children

--- components/Repos.jsx
+++ components/Repos.jsx
@@ -5,13 +5,13 @@ export default React.createClass({
   // <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
   // <li><NavLink to="/repos/facebook/react">React</NavLink></li>
 
+  // {this.props.children}
   render() {
     return (
       <div>
         <h2 className="content">Repos</h2>
         <ul className="content">
         </ul>
-        {this.props.children}
       </div>
     )
   }
