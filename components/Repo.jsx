27ead79786d1357155c9e3221226a1411150847b
BKhurshid <josephsun4@gmail.com>
2016-04-10 19:35:05 -0700

    commenting out code

--- components/Repo.jsx
+++ components/Repo.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
+// <h2 className="content">{this.props.params.repoName}</h2>
 export default React.createClass({
   render() {
     return (
       <div>
 
-        <h2 className="content">{this.props.params.repoName}</h2>
       </div>
     )
   }
